42cc5e3771e93535675d8d1b77759e365d653cbc
Gerd Zschaler <gerd@gzschaler.de>
2015-06-07 21:54:22 +0200

    Add ApiConfig

--- test/api_config.js
+++ test/api_config.js
@@ -1,21 +1,37 @@
 describe('chrome.cast.ApiConfig', function() {
-    var ApiConfig;
+    var chrome = require('../index'), apiConfig;
 
     beforeEach(function() {
-        var chrome = require('../index');
-        ApiConfig = chrome.cast.ApiConfig;
+        apiConfig = new chrome.cast.ApiConfig();
     });
 
-    it('should be defined', function() {
-        expect(ApiConfig).to.be.a('function');
+    it('should provide the required fields', function() {
+        [
+            'autoJoinPolicy',
+            'defaultActionPolicy',
+            'receiverListener',
+            'sessionListener',
+            'sessionRequest'
+        ].forEach(function(field) {
+                expect(apiConfig).to.have.property(field);
+            });
     });
 
-    it('should provide the configured data', function() {
-        var apiConfig = new ApiConfig();
-        expect(apiConfig).to.have.property('autoJoinPolicy');
-        expect(apiConfig).to.have.property('defaultActionPolicy');
-        expect(apiConfig).to.have.property('receiverListener');
-        expect(apiConfig).to.have.property('sessionListener');
-        expect(apiConfig).to.have.property('sessionRequest');
+    it('should set required default values', function() {
+        expect(apiConfig.autoJoinPolicy).to.equal(chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED);
+        expect(apiConfig.defaultActionPolicy).to.equal(chrome.cast.DefaultActionPolicy.CREATE_SESSION);
+    });
+
+    it('should store input values', function() {
+        var sessionRequest = {},
+            sessionListener = function sessionListener() {},
+            receiverListener = function receiverListener() {};
+        apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener,
+            chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED, chrome.cast.DefaultActionPolicy.CAST_THIS_TAB);
+        expect(apiConfig.sessionRequest).to.equal(sessionRequest);
+        expect(apiConfig.sessionListener).to.equal(sessionListener);
+        expect(apiConfig.receiverListener).to.equal(receiverListener);
+        expect(apiConfig.autoJoinPolicy).to.equal(chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED);
+        expect(apiConfig.defaultActionPolicy).to.equal(chrome.cast.DefaultActionPolicy.CAST_THIS_TAB);
     });
 });
